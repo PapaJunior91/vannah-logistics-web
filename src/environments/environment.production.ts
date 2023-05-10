16d4156ce4615400d2a43df0643e27b724886844
Nkambwe Swaibu <56865915+PapaJunior91@users.noreply.github.com>
2023-05-10 18:00:42 +0300

    Update environment.production.ts change base url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production:true,
-    baseUrl: 'https://tms-api.server.nexent.dev/api/'
+    //baseUrl: 'https://tms-api.server.nexent.dev/api/'
+    baseUrl: 'https://vannah-logistics-api.deploy.nkambwe.com/api/'
 };
